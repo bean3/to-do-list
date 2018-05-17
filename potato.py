b6ffd14bf0686d4fe9e7ba581443e2b50ad854c9
xodh <kto6278@naver.com>
2018-05-17 16:25:16 +0900

    Update potato.py

--- potato.py
+++ potato.py
@@ -1,5 +1,6 @@
 import sqlite3
 import click
+import sys
 import re
 
 def create_db():
@@ -13,16 +14,32 @@ def create_db():
                 id integer primary key autoincrement,
                 what text not null,
                 due text not null,
-                finished integer default 0);"""
+                finished integer default 0,
+                validation integer);"""
     
     cur.execute(table_create_sql)
 
 def exe_mk(mk):
+    # due 가 정규식에 맞지 않으면 다시 입력 받는다.
+    due = str(mk[1])
+    regular = re.compile(r"(\d{4})[-](\d{2})[-](\d{2})[/](\d{2})[:](\d{2})")
+    while True:
+        if len(due) != 16:
+            print("Please type in the right format : YYYY-MM-DD HH:MM")
+            sys.exit(1)
+        match = regular.match(due)
+        if match == None:
+            print("Please type in the right format : YYYY-MM-DD HH:MM")
+            sys.exit(1)
+        else:
+            break
+    due = regular.match(mk[1])
+
+    what = str(mk[0])
 
     sql = "insert into todo (what, due) values (?, ?)"
-    task = mk
 
-    cur.execute(sql, task)
+    cur.execute(sql, (what, due.group(0),))
     conn.commit()
 
 def exe_rm(rm):
@@ -40,7 +57,7 @@ def exe_mod(mod):
     cur.execute("select * from todo where id=?", (mod,))
     row = cur.fetchall()
 
-    if mod.isdigit() and row and int(mod) <= len(rows) and mod > '0':
+    if mod.isdigit() and row and int(mod)<=len(rows) and mod > '0':
         print("\n(Nothing will change if you enter nothing.)")
 
         wh = str(input("What's your new plan?: "))
@@ -95,40 +112,38 @@ def print_list(p_opt):
 
     cur.execute(check_print_option(p_opt))
     rows = cur.fetchall()
+    print(
+        "\n"
+        "                    P O T A T O F I E L D                  \n"
+        "============================================================\n"
+        "| No.|   Description   |         Due         |   Status    |\n"
+        "============================================================"
+        )
+    for row in rows:
 
-    if rows:
+        #Get the columns
+        num = row[0]
+        wh = row[1]
+        du = row[2]
+        fin = row[3]
 
         print(
-            "\n"
-            "                   P O T A T O F I E L D                 \n"
-            "=========================================================\n"
-            "| No.|   Description   |       Due        |   Status    |\n"
-            "========================================================="
+            #Print the number of plan; Max : 2-digit number
+            "|", str(num).ljust(2),
+            #Print @#$^... if the description is too long to print; Max : 15 chars
+            "|", wh.ljust(15) if len(wh)<=15 else wh[:12] + "...",
+            #Print the due as it is since the due has its own format; YYYY-MM-DD
+            "|", du,
+            #Print whether the plan is done or not
+            "| Done        |" if fin==1 else "| In progress |"
             )
 
-        for row in rows:
-
-            #Get the columns
-            num = row[0]
-            wh = row[1]
-            du = row[2]
-            fin = row[3]
-
-            print(
-                #Print the number of plan; Max : 2-digit number
-                "|", str(num).ljust(2),
-                #Print @#$^... if the description is too long to print; Max : 15 chars
-                "|", wh.ljust(15) if len(wh)<=15 else wh[:12] + "...",
-                #Print the due as it is since the due has its own format; YYYY-MM-DD
-                "|", du,
-                #Print whether the plan is done or not
-                "| Done        |" if fin==1 else "| In progress |"
-                )
-
         print(
-            "=========================================================\n"
-            #Dummy line, but planning to make pages
-            "                                              Page 01/01 \n")
+
+        "============================================================\n"
+        #Dummy line, but planning to make pages
+        "                                                 Page 01/01 \n")
+
 
     else:
         print("Nothing to print :(\n")
@@ -155,6 +170,7 @@ def run(mk, rm, mod, p_opt):
         exe_mod(mod)
     elif p_opt:
         print_option = p_opt
+
 
     print_list(print_option)
     conn.close()
