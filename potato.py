a8cc53f36f7348a6dc808cdf3cf00d8bd5535646
jesin <audgus0914@naver.com>
2018-05-18 08:56:56 +0900

    no message

--- potato.py
+++ potato.py
@@ -57,7 +57,7 @@ def exe_mod(mod):
         conn.commit()
 
     else:
-    	print("\nInvalid number; check it again! :(")
+        print("\nInvalid number; check it again! :(")
 
 def check_print_option(p_opt):
 
@@ -75,42 +75,17 @@ def print_list(p_opt):
     cur.execute(check_print_option(p_opt))
     rows = cur.fetchall()
 
+    print("")
     if rows:
-
-        print(
-            "\n"
-            "               P O T A T O F I E L D               \n"
-            "===================================================\n"
-            "| No.|   Description   |    Due     |   Status    |\n"
-            "==================================================="
-            )
-
         for row in rows:
-
-            #Get the columns
-            num = row[0]
-            wh = row[1]
-            du = row[2]
-            fin = row[3]
-
-            print(
-                #Print the number of plan; Max : 2-digit number
-                "|", str(num).ljust(2),
-                #Print @#$^... if the description is too long to print; Max : 15 chars
-                "|", wh.ljust(15) if len(wh)<=15 else wh[:12] + "...",
-                #Print the due as it is since the due has its own format; YYYY-MM-DD
-                "|", du,
-                #Print whether the plan is done or not
-                "| Done        |" if fin==1 else "| In progress |"
-                )
-
-        print(
-            "===================================================\n"
-            #Dummy line, but planning to make pages
-            "                                        Page 01/01 \n")
-
+            for n in range(0,4):
+                if(n<3):
+                    print(" | " + str(row[n]), end="")
+                else:
+                    print(" | Done |" if row[3]==1 else " | In progress |", end="")
+            print("")
     else:
-        print("Nothing to print :(\n")
+        print("Nothing to print :(")
 
 @click.command()
 #Basic options
