be9189aecd584fbe9ed84e61b52574a82818b437
Hyunjun Lee <31719859+Alpacadabra@users.noreply.github.com>
2018-05-29 19:39:08 +0900

    Update setup.py
    Since we used f-string, it requires Python version 3.6 or later.

--- setup.py
+++ setup.py
@@ -15,11 +15,6 @@ setup(
 	    ]
 	},
 	classifiers      = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ]
 )
