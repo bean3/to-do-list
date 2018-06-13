f6ebf051fc67ca517698f6d4d2cfee893552550f
Alpacadabra <>
2018-06-14 00:19:13 +0900

    New test cases;

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 import sqlite3
-import os
 import subprocess
-
-def test_remove():
-	os.system('')
 	
 def test_make():
-	result = subprocess.check_output('potato make --what icecream --due 2018-06-29/12:00 --cat chores')
-	assert result == b'\r\nYour plan has been successfully added!\r\n\r\n'
+	result = subprocess.check_output('potato make --what icecream --due 2018-06-29/12:00 --cat chores', shell=True)
+	assert result == b'\nYour plan has been successfully added!\n\n'
+
+def test_remove():
+	result = subprocess.check_output('potato remove --all', shell=True)
+	assert result == b'\nYour plans have been successfully removed!\n\n'
 
 def test_print():
-	result = subprocess.check_output('potato show')
-	assert result == b'                         P O T A T O F I E L D                         \r\n=======================================================================\r\n| No. |   Description   |       Due        |  Category  |   Status    |\r\n=======================================================================\r\n| 1   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n| 2   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n| 3   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n=======================================================================\r\n Category : ALL                                           Page 001/001 \r\n'
+	result = subprocess.check_output('potato show', shell=True)
+	assert result == b'\nNothing to print :(\n\n'
 
 def main():
 	test_make()
+	test_remove()
 	test_print()
 
 if __name__ == '__main__':
