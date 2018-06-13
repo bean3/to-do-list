e14bd2f318298f5ddd7199518fc69827aebb4d14
Hyunjun Lee <31719859+Alpacadabra@users.noreply.github.com>
2018-06-14 00:06:10 +0900

    moving to mac os;

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
+import sqlite3
 import os
+import subprocess
 
+def test_remove():
+	os.system('')
+	
 def test_make():
-	os.system('potato make --what icecream --due 2018-06-29/12:00 --cat chores')
+	result = subprocess.check_output('potato make --what icecream --due 2018-06-29/12:00 --cat chores')
+	assert result == b'\r\nYour plan has been successfully added!\r\n\r\n'
 
 def test_print():
-	assert os.system('potato show') == "                         P O T A T O F I E L D                         \n"+
-"=======================================================================\n"+
-"| No. |   Description   |       Due        |  Category  |   Status    |\n"+
-"=======================================================================\n"+
-"| 1   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\n"+
-"=======================================================================\n"+
-" Category : ALL                                           Page 001/001"
+	result = subprocess.check_output('potato show')
+	assert result == b'                         P O T A T O F I E L D                         \r\n=======================================================================\r\n| No. |   Description   |       Due        |  Category  |   Status    |\r\n=======================================================================\r\n| 1   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n| 2   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n| 3   | icecream        | 2018-06-29/12:00 | chores     | In Progress |\r\n=======================================================================\r\n Category : ALL                                           Page 001/001 \r\n'
 
 def main():
 	test_make()
